6d3bd6301d80eec2bd92f39b6aa58b3d766abf2d
vaishnavivignesh <148718139+vaishnavivignesh@users.noreply.github.com>
2023-10-23 03:15:05 +0530

    Update WordCounterServiceImplTest.java

--- src/test/java/com/sample/wordcounter/serviceimpl/WordCounterServiceImplTest.java
+++ src/test/java/com/sample/wordcounter/serviceimpl/WordCounterServiceImplTest.java
@@ -4,12 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -28,26 +24,51 @@ public class WordCounterServiceImplTest {
 	ValidationService validationservice;
 
 	@Test
-	public void addWordTest() {
+	public void addWordTestSuccess() {
 
-		List<String> localValue = Arrays.asList("London", "Glasgow");
+		List<String> localValue = Arrays.asList("Edinburgh","Cambridge" ,"London", "Glasgow");
 		when(validationservice.validateInput(any())).thenReturn(localValue);
 		String result = wordCounterServiceImpl.addWord(buildWordObject());
 		assertEquals(result, "SUCCESS");
 
 	}
+	
+	@Test
+	public void addWordTestpartialsuccess() {
+
+		List<String> localValue = Arrays.asList("Cambridge" ,"London", "Glasgow");
+		when(validationservice.validateInput(any())).thenReturn(localValue);
+		String result = wordCounterServiceImpl.addWord(buildWordObject());
+		assertEquals(result, "PARTIAL_SUCCESS");
+
+	}
+	
+	@Test
+	public void addWordTestFailure() {
+
+		List<String> localValue = null;
+		when(validationservice.validateInput(any())).thenReturn(localValue);
+		String result = wordCounterServiceImpl.addWord(buildWordObject());
+		assertEquals(result, "FAILURE");
+
+	}
+	
+	@Test
+	public void countWordFound() {
+		wordCounterServiceImpl.countWord("London");
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void countWordNotFound() {
+		wordCounterServiceImpl.countWord("America");
+		assertEquals(0,0);
+	}
 
 	private Word buildWordObject() {
 
-		Word word = new Word("Edinburgh Cambridge", Arrays.asList("London", "Glasgow"));
+		Word word = new Word(null, Arrays.asList("Edinburgh", "Cambridge", "London", "Glasgow"));
 		return word;
 	}
-
-	/*
-	 * @BeforeEach public void setup() {
-	 * validationservice.validateInput(buildWordObject());
-	 * 
-	 * }
-	 */
 
 }
